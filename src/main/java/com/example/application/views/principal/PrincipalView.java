9812e0fcf8175c64e874b1d8055e6a5e607c6956
start.vaadin.com <no-reply@vaadin.com>
2022-02-10 21:38:36 +0000

    Generated project

---
+++ src/main/java/com/example/application/views/principal/PrincipalView.java
@@ -0,0 +1,36 @@
+package com.example.application.views.principal;
+
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.Theme;
+
+@PWA(name = "Dr Strange 2", shortName = "Dr Strange 2", enableInstallPrompt = false)
+@Theme(themeFolder = "drstrange2")
+@PageTitle("Principal")
+@Route(value = "principal")
+@RouteAlias(value = "")
+public class PrincipalView extends VerticalLayout {
+
+    public PrincipalView() {
+        setSpacing(false);
+
+        Image img = new Image("images/empty-plant.png", "placeholder plant");
+        img.setWidth("200px");
+        add(img);
+
+        add(new H2("This place intentionally left empty"));
+        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+
+        setSizeFull();
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        getStyle().set("text-align", "center");
+    }
+
+}
